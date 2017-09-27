8be97d7addb8d72c1491c8fed3fd3cafaa59939d
omsk34 <omsk34@ptc.local>
2017-09-27 10:42:18 +0600

    vd

---
+++ src/main/java/Raiffeisen/Belly.java
@@ -0,0 +1,7 @@
+package Raiffeisen;
+
+public class Belly {
+    public void eat(int cukes) {
+
+    }
+}
---
+++ src/test/java/skeleton/RunCukesTest.java
@@ -0,0 +1,12 @@
+package skeleton;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"},
+features="src/main/java/Raiffeisen/Belly.java")
+public class RunCukesTest {
+
+}
